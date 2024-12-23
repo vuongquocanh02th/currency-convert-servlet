ee05611f9e3fa5a5e3bb0f98d45df63b625f04ea
Vuong Quoc Anh <vuongquocanh300402@gmail.com>
2024-12-23 14:55:20 +0700

    chuyen doi tien te

---
+++ src/main/java/org/example/currencyconvertservlet/ConverterServlet.java
@@ -0,0 +1,27 @@
+package org.example.currencyconvertservlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ConverterServlet", value = "/convert")
+public class ConverterServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        float rate = Float.parseFloat(req.getParameter("rate"));
+        float usd = Float.parseFloat(req.getParameter("usd"));
+
+        float vnd = rate * usd;
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>Rate: " + rate + "</h1>");
+        writer.println("<h1>USD: " + usd + "</h1>");
+        writer.println("<h1>VND: " + vnd + "</h1>");
+        writer.println("</html>");
+    }
+}
